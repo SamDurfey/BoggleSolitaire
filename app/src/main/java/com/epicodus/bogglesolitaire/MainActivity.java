cdd5ae3213eede45887d827f1ffc9575e13d368c
Joey Arnstein and Samuel Durfey <samuel@>
2016-06-28 16:13:19 -0700

    Button now creates a gridview of random letters.

--- app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -15,8 +18,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.titleTextView) TextView mTitleTextView;
-    @Bind(R.id.randomLetters) TextView mRandomLetters;
+//    @Bind(R.id.randomLetters) TextView mRandomLetters;
     @Bind(R.id.button) Button mButton;
+    @Bind(R.id.gridview) GridView mGridView;
     private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private Random randGen = new Random();
     private String[] boggleLetters = new String[8];
@@ -39,8 +43,10 @@ public class MainActivity extends AppCompatActivity {
                     String letter = String.valueOf(alphabet.charAt(randGen.nextInt(25)));
                     boggleLetters[index] = letter;
                 }
-//                Log.d("Result: ", Arrays.toString(boggleLetters));
-                mRandomLetters.setText(Arrays.toString(boggleLetters));
+                Log.d("Result: ", Arrays.toString(boggleLetters));
+//                mRandomLetters.setText(Arrays.toString(boggleLetters));
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, boggleLetters);
+                mGridView.setAdapter(adapter);
             }
         });
     }
